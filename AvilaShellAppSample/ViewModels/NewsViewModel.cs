8d685e685c57793c25a1f5f58b66b6485aa68338
Pierre-Christophe <pc.dus@laposte.net>
2020-09-04 16:01:32 +0200

    Add News page + Facebook news implementation

---
+++ AvilaShellAppSample/ViewModels/NewsViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AvilaShellAppSample.Services;
+using AvilaShellAppSample.Models;
+using System.Diagnostics;
+
+namespace AvilaShellAppSample.ViewModels
+{
+    public class NewsViewModel : AvilaViewModelBase
+    {
+
+        private readonly ApiFacebookGraphService _facebookApi;
+
+        FacebookFeedDTO fbNews = null;
+        public FacebookFeedDTO FbNews
+        {
+            get { return fbNews; }
+            set { SetProperty(ref fbNews, value); }
+        }
+
+
+        public NewsViewModel()
+        {
+            Title = "News";
+            _facebookApi = new ApiFacebookGraphService();
+
+            //Task.Run(async () => { await GetFbNewsAsync(); }).Wait();
+            Task.Run(async () => await GetFbNewsAsync());
+        }
+
+
+        private async Task GetFbNewsAsync()
+        {
+            fbNews = await _facebookApi.GetFacebookNewsAsync();
+            if (fbNews == null)
+            {
+                Debug.WriteLine("null");
+            }
+            else
+            {
+                Debug.WriteLine("not null");
+            }
+        }
+    }
+}
+
---
+++ AvilaShellAppSample/Views/NewsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AvilaShellAppSample.Views
+{
+    public partial class NewsPage : ContentPage
+    {
+        public NewsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
