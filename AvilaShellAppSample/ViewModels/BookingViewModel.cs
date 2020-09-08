9f1a177b348025b9cd6f6785a09ad4f52c84e6d8
Pierre-Christophe <pc.dus@laposte.net>
2020-09-08 18:14:33 +0200

    Update Booking page

--- AvilaShellAppSample/ViewModels/BookingViewModel.cs
+++ AvilaShellAppSample/ViewModels/BookingViewModel.cs
@@ -1,11 +1,36 @@
 ﻿using System;
+using System.Threading.Tasks;
+using MvvmHelpers.Commands;
+
 namespace AvilaShellAppSample.ViewModels
 {
     public class BookingViewModel : AvilaViewModelBase
     {
+        string avilaUrlBooking = "https://booking.wavy.pro/avila";
+        public string AvilaUrlBooking
+        {
+            get { return avilaUrlBooking; }
+            set { SetProperty(ref avilaUrlBooking, value); }
+        }
+
+        public Command RefreshCommand => new Command(this.Refresh);
+
         public BookingViewModel()
         {
             Title = "Booking";
+        }
+
+        public void Refresh(object sender)
+        {
+            try
+            {
+                var view = sender as Xamarin.Forms.WebView;
+                view.Reload();
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
--- AvilaShellAppSample/Views/BookingPage.xaml.cs
+++ AvilaShellAppSample/Views/BookingPage.xaml.cs
@@ -7,10 +7,17 @@ namespace AvilaShellAppSample.Views
 {
     public partial class BookingPage : ContentPage
     {
+
         public BookingPage()
         {
             InitializeComponent();
-            webView.Source = "https://booking.wavy.pro/avila";
+            //webView.Source = "https://booking.wavy.pro/avila";
         }
+
+        void ToolbarItem_Clicked(System.Object sender, System.EventArgs e)
+        {
+            this.webView.Reload();
+        }
+
     }
 }
